696ebed28b09c543ecbc5d429e9a917a543124bc
Alejandro Chitiva Castillo <alejochitiva@gmail.com>
2023-02-25 22:33:30 -0500

    CSV File readed and file records readed

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -13,6 +13,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.FileReader;
 import java.util.HashMap;
@@ -22,11 +23,11 @@ import java.util.Map;
 public class AplMain {
 
     public static void main(String[] args) {
-
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.DOWN);
         try {
             File archivo = new File("src/archivo-1.txt");
             // formatting the decimals to 2 places
-            DecimalFormat df = new DecimalFormat("#.##");
             df.setRoundingMode(RoundingMode.DOWN);
             double s = archivo.length();
             String size = df.format(archivo.length());
@@ -41,7 +42,7 @@ public class AplMain {
             JOptionPane.showMessageDialog(null, "El tamaño del archivo es: " + mb + " Megabytes");
             scanner.close();
             //Archivo CSV de Salida
-            String archivoCsv = "archivo-1.csv";
+            String archivoCsv = "src/archivo-1.csv";
             try (BufferedReader lector = new BufferedReader(new FileReader(archivo));
                  FileWriter writer = new FileWriter(archivoCsv)) {
                 // Leer cada línea del archivo de texto y escribir en el archivo CSV
@@ -49,7 +50,6 @@ public class AplMain {
                 while ((linea = lector.readLine()) != null) {
                     // Dividir la línea en campos utilizando el separador adecuado
                     String[] campos = linea.split(",");
-
                     // Escribir los campos en el archivo CSV
                     for (int i = 0; i < campos.length; i++) {
                         writer.append(campos[i]);
@@ -60,24 +60,119 @@ public class AplMain {
                     writer.append("\n");
                 }
                 JOptionPane.showMessageDialog(null, "El archivo se ha convertido a CSV correctamente.");
-//                //Cargar el archivo CSV
-//                File archivoCSV = new File("archivo-1.csv");
-//                Scanner scannerCSV = new Scanner(archivoCSV);
-//                while (scannerCSV.hasNextLine()) {
-//                    String lineaCSV = scannerCSV.nextLine();
-//                    System.out.println(lineaCSV);
-//                }
-//                scannerCSV.close();
-
             } catch (IOException e) {
                 e.printStackTrace();
                 JOptionPane.showMessageDialog(null, "Error al leer o escribir el archivo: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             }
-
         } catch (FileNotFoundException e) {
             JOptionPane.showMessageDialog(null, "El archivo no se encontró.", "Error", JOptionPane.ERROR_MESSAGE);
             e.printStackTrace();
         }
+        // Crear un objeto File para representar el archivo CSV
+        File archivo = new File("src/archivo-1.csv");
+
+        // Crear un objeto Scanner para leer el archivo CSV
+        Scanner scanner;
+        try {
+            scanner = new Scanner(archivo);
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo no existe.");
+            return;
+        }
+
+        // Crear un objeto HashMap para almacenar los contadores por género
+        Map<String, Integer> contadoresGenero = new HashMap<>();
+        Map<String, Integer> contadoresZona = new HashMap<>();
+        Map<String, Integer> contadoresMes = new HashMap<>();
+        Map<String, Integer> contadoresDepartamento = new HashMap<>();
+        Map<String, Integer> contadoresEdad = new HashMap<>();
+        int totalDelitos = 0;
+
+        // Leer cada línea del archivo CSV y contar los delitos por género
+        while (scanner.hasNextLine()) {
+            String linea = scanner.nextLine();
+            String[] campos = linea.split(",");
+
+            if (campos.length == 13) {
+//                System.out.println(campos[2].trim());
+                String genero = campos[8];
+                String zona = campos[7];
+                String mes = campos[2].split("/")[0];
+                String departamento = campos[0];
+                String edad = campos[11];
+                // Contar delitos por género
+                Integer contadorGenero = contadoresGenero.getOrDefault(genero, 0);
+                contadoresGenero.put(genero, contadorGenero + 1);
+
+                // Contar delitos por zona
+                Integer contadorZona = contadoresZona.getOrDefault(zona, 0);
+                contadoresZona.put(zona, contadorZona + 1);
+
+                // Contar delitos por mes
+                Integer contadorMes = contadoresMes.getOrDefault(mes, 0);
+                contadoresMes.put(mes, contadorMes + 1);
+
+                // Contar delitos por departamento
+                Integer contadorDepartamento = contadoresDepartamento.getOrDefault(departamento, 0);
+                contadoresDepartamento.put(departamento, contadorDepartamento + 1);
+
+                // Contar delitos por edad
+                Integer contadorEdad = contadoresEdad.getOrDefault(edad, 0);
+                contadoresEdad.put(edad, contadorEdad + 1);
+
+                totalDelitos++;
+            }
+        }
+
+        // Calcular el porcentaje de delitos por género y mostrar los resultados
+        System.out.println("Delitos por género");
+        for (String genero : contadoresGenero.keySet()) {
+            int cantidad = contadoresGenero.get(genero);
+            double porcentaje = (double) cantidad / totalDelitos * 100;
+            String percent= df.format(porcentaje);
+            System.out.println("Género: " + genero + ", cantidad de delitos: " + cantidad + ", porcentaje: " + percent + "%");
+        }
+        System.out.println("-----------------------------------------------------------");
+
+        System.out.println("Delitos por zona");
+        for (String zona : contadoresZona.keySet()) {
+            int cantidad = contadoresZona.get(zona);
+            double porcentaje = (double) cantidad / totalDelitos * 100;
+            String percent= df.format(porcentaje);
+            System.out.println("Zona: " + zona + ", cantidad de delitos: " + cantidad + ", porcentaje: " + percent + "%");
+        }
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Delitos por mes");
+        for (String mes : contadoresMes.keySet()) {
+            int cantidad = contadoresMes.get(mes);
+            double porcentaje = (double) cantidad / totalDelitos * 100;
+            String percent= df.format(porcentaje);
+            System.out.println("Mes: " + mes + ", cantidad de delitos: " + cantidad + ", porcentaje: " + percent + "%");
+        }
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Delitos por departamento");
+        for (String departamento : contadoresDepartamento.keySet()) {
+            int cantidad = contadoresDepartamento.get(departamento);
+            double porcentaje = (double) cantidad / totalDelitos * 100;
+            String percent= df.format(porcentaje);
+            System.out.println("Departamento: " + departamento + ", cantidad de delitos: " + cantidad + ", porcentaje: " + percent + "%");
+        }
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Delitos por edad");
+        for (String edad : contadoresEdad.keySet()) {
+            int cantidad = contadoresEdad.get(edad);
+            double porcentaje = (double) cantidad / totalDelitos * 100;
+            String percent= df.format(porcentaje);
+            System.out.println("Edad: " + edad + ", cantidad de delitos: " + cantidad + ", porcentaje: " + percent + "%");
+        }
+        System.out.println("-----------------------------------------------------------");
+        JOptionPane.showMessageDialog(null,"Total de delitos: " + totalDelitos);
+        System.out.println("Total de delitos: " + totalDelitos);
+
+        scanner.close();
+
+
+
     }
 }
 

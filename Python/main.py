014172e9b88d13e394a9a0663d41cae595bf21d4
Alejandro Chitiva Castillo <alejochitiva@gmail.com>
2023-03-04 22:45:47 -0500

    Pandas Libraries added

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,16 @@
-import codecs
+import pandas as pd
+import pandas_profiling as pp
 
-with codecs.open('TestFile.txt', 'r', encoding='utf-8', errors='ignore') as f:
-    contenido = f.read()
-    print(contenido)
+archivo_txt = '../src/delitos(sáb. 04-03-2023).txt'
+delimiter = '\t'
+header = None
+
+df = pd.read_csv(archivo_txt, delimiter=delimiter, header=header, encoding='utf-8')
+
+print(df.columns.values)
+print("------------Primeros 5 registros------------", df.head(5))
+print("------------Últimos 10 registros------------", df.tail(10))
+print(df.describe())
+
+report = pp.ProfileReport(df)
+report.to_file(output_file='output.html')
